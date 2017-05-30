37a1a872c272ae3300e10de17490eee95ac2289e
J K <jonathanwork@protonmail.com>
2017-05-30 16:05:09 +0100

    done bulk of things for fb login, need banner...

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530094628) do
+ActiveRecord::Schema.define(version: 20170530143947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 20170530094628) do
     t.string   "skills"
     t.string   "city"
     t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
@@ -57,5 +64,4 @@ ActiveRecord::Schema.define(version: 20170530094628) do
   add_foreign_key "contracts", "services"
   add_foreign_key "contracts", "users"
   add_foreign_key "services", "users"
-
 end
